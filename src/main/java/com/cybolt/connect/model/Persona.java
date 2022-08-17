8056ec9e6d8cbe7da63b3d51f25104663d72c961
SalvadorArce <chavitaarce@gmail.com>
2022-08-17 15:29:08 -0500

    Subiendo actualizacion...

--- src/main/java/com/cybolt/connect/model/Persona.java
+++ src/main/java/com/cybolt/connect/model/Persona.java
@@ -1,5 +1,47 @@
 package com.cybolt.connect.model;
 
 public class Persona {
-
+	 private int identificador;
+	  private String nombre;
+	  private String apellidop;
+	  private String apellidom;
+	  private String direccion;
+	  private String municipio;
+	  
+	  	public int getIdentificador(){
+	  		return identificador;
+	    }
+	    public void setIdentificador(int identificador){
+	        this.identificador= identificador;
+	    }
+	    public String getNombre(){
+	        return nombre;
+	    }
+	    public void setNombre(String nombre){
+	        this.nombre =nombre;
+	    }
+	    public String getApellidop() {
+	    	return apellidop;
+	    }
+	    public void setApellidop(String apellidop) {
+	    	this.apellidop=apellidop;
+	    }
+	    public String getApellidom() {
+	    	return apellidom;
+	    }
+	    public void setApellidom(String apellidom) {
+	    	this.apellidom=apellidom;
+	    }
+	    public String getDireccion() {
+	    	return direccion;
+	    }
+	    public void setDireccion(String direccion) {
+	    	this.direccion=direccion;
+	    }
+	    public String getMunicipio() {
+	    	return municipio;
+	    }
+	    public void setMunicipio(String municipio) {
+	    	this.municipio=municipio;
+	    }
 }
--- src/main/java/com/cybolt/connect/model/Ubicacion.java
+++ src/main/java/com/cybolt/connect/model/Ubicacion.java
@@ -2,4 +2,47 @@ package com.cybolt.connect.model;
 
 public class Ubicacion {
 
-}
+	 private int id_ubicacion;
+	 private int id_investigacion;
+	 private String latitud;
+	 private String longitud;
+	 private String pais;
+	 private String municipio;
+	 
+	 	public int getIdubicacion() {
+	 		return id_ubicacion;
+	 	}
+	 	public void setIdubicacion(int id_ubicacion) {
+	 		this.id_ubicacion=id_ubicacion;
+	 	}
+	 	public int getIdinvestigacion() {
+	 		return id_investigacion;
+	 	}
+	 	public void setIdinvestigacion(int id_investigacion) {
+	 		this.id_investigacion=id_investigacion;
+	 	}
+	 	public String getLatitud() {
+	 		return latitud;
+	 	}
+	 	public void setLatitud(String latitud) {
+	 		this.latitud=latitud;
+	 	}
+	 	public String getLongitud() {
+	 		return longitud;
+	 	}
+	 	public void setLongitud(String longitud) {
+	 		this.longitud=longitud;
+	 	}
+	 	public String getPais() {
+	 		return pais;
+	 	}
+	 	public void setPais(String pais) {
+	 		this.pais=pais;
+	 	}
+	 	public String getMunicipio() {
+	 		return municipio;
+	 	}
+	 	public void setMunicipio(String municipio) {
+	 		this.municipio=municipio;
+	 	}
+   }
